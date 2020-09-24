aa7487295276b20c150bd55bc5a2a596641ef510
Mason Waters <masonwaters80@yahoo.com>
2020-09-23 20:56:34 -0500

    lex.c change

--- lex.c
+++ lex.c
@@ -4,21 +4,21 @@ char *_args[10];
 int _argcount = 0;
 %}
 
-WORD	[a-zA-Z0-9\/\.-]+
-SPECIAL	[()><|&;*]
+WORD    [a-zA-Z0-9\/\.-]+
+SPECIAL [()><|&;*]
 
 %%
-	_argcount = 0; 
-	_args[0] = NULL; 
+        _argcount = 0;
+        _args[0] = NULL;
 
-{WORD}|{SPECIAL} {  
-	  if(_argcount < _numargs-1) {
-	    _args[_argcount++] = (char *)strdup(yytext);
-	    _args[_argcount] = NULL;
-	  }
-	}
+{WORD}|{SPECIAL} {
+          if(_argcount < _numargs-1) {
+            _args[_argcount++] = (char *)strdup(yytext);
+            _args[_argcount] = NULL;
+          }
+        }
 
-\n	return (int)_args;
+\n      return (uintptr_t)_args;//we are replacing (int) with uintptr_t
 
 [ \t]+
 
@@ -26,8 +26,6 @@ SPECIAL	[()><|&;*]
 
 %%
 
-char **getline() {
-  return (char **)yylex();
+uintptr_t getaline() {//changed this function name from getline() to getaline()
+  return (uintptr_t)yylex();//we are replacing (char **) with (uintptr_t)
 }
-
-
